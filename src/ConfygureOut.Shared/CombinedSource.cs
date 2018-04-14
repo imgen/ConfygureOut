c36dad5f76c3677c4949d2280dd9ddf60360d41b
Imgen <imgen@hotmail.com>
2018-04-14 08:28:04 +0800

    Implement CombinedSource feature

---
+++ src/ConfygureOut.Shared/CombinedSource.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConfygureOut.Shared
+{
+    public class CombinedSource: BaseConfigurationSource
+    {
+        private readonly List<BaseConfigurationSource> _sources;
+        public CombinedSource(string name, 
+                              bool supportHotLoad, 
+                              params BaseConfigurationSource[] sources)
+            :base(name, supportHotLoad)
+        {
+            _sources = sources.ToList();
+        }
+        
+        public CombinedSource(string name, 
+            bool supportHotLoad, 
+            IEnumerable<BaseConfigurationSource> sources)
+            :this(name, supportHotLoad, sources.ToArray())
+        {
+        }
+
+		public override async Task LoadConfigurations()
+		{
+            foreach(var source in _sources)
+            {
+                await source.LoadConfigurations();
+            }
+		}
+
+		public override object GetConfigurationValue(string key, Type propertyType)
+		{
+            object value = null;
+            foreach(var source in _sources)
+            {
+                value = source.GetConfigurationValue(key, propertyType);
+                if (value != ConfigurationValueNotFound.Instance)
+                {
+                    break;
+                }
+            }
+
+            return value;
+		}
+	}
+}
---
+++ src/ConfygureOut.Shared/CombinedSourceUseExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using ConfygureOut.Shared;
+
+namespace ConfygureOut
+{
+    public static class CombinedSourceUseExtension
+    {
+        public static BaseConfiguration UseCombinedSource(
+            this BaseConfiguration configuration,
+            string name,
+            bool supportHotLoad,
+            IEnumerable<BaseConfigurationSource> sources,
+            TimeSpan? autoRefreshInterval = null
+         )
+        {
+            var combinedSource = new CombinedSource(name, supportHotLoad, sources);
+            return configuration.RegisterConfigurationSources((combinedSource, autoRefreshInterval));
+        }
+    }
+}
